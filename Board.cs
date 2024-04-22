2f0f7cc754262a10aebac7ab78d53e6903e7a114
Micah Ma <701004018@GoTVDSB.ca>
2024-04-22 13:20:02 -0400

    Add project files.

---
+++ Board.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Minesweeper
+{
+    internal class Board
+    {
+        private int _width, _height, _numMines;
+        private Texture2D _rectTexture;
+        private SpriteFont _font;
+        private MouseState _mouseState, _prevMouseState;
+        private List<Rectangle> _rects;
+        public Board(Texture2D texture, int width, int height, int numMines, SpriteFont font)
+        {
+            _width = width;
+            _height = height;
+            
+            _numMines = numMines;
+            _rectTexture = texture;
+
+            _font = font;
+        }
+        private void NewBoard()
+        {
+
+        }
+        public void Update(MouseState mousestate, MouseState prevmousestate)
+        {
+            _mouseState = mousestate;
+            _prevMouseState = prevmousestate;
+
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Minesweeper
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        int width, height;
+
+        Texture2D rectTexture,flagTexture,mineTexture,explodeTexture,heartTexture,coinTexture,restartTexture;
+
+        MouseState mouseState, prevMouseState;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = GraphicsDevice.Adapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsDevice.Adapter.CurrentDisplayMode.Height;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+            width = _graphics.PreferredBackBufferWidth;
+            height = _graphics.PreferredBackBufferHeight;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            flagTexture = Content.Load<Texture2D>("flag");
+            rectTexture = Content.Load<Texture2D>("rectangle");
+            mineTexture = Content.Load<Texture2D>("mine");
+            explodeTexture = Content.Load<Texture2D>("exploded");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
